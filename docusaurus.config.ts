5a05ff89206b4f52213fe83db57f7e9c76a80484
cobeo2004 <minh030963@stu.vinschool.edu.vn>
2024-07-03 00:42:03 +0700

    fix bug

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,13 +8,14 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://cobeo2004.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/react-js-doc/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
+
   organizationName: 'cobeo2004', // Usually your GitHub org/user name.
   projectName: 'react-js-doc', // Usually your repo name.
 
